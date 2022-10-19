3080ba33bbfb3cf86b1bfc85a3204ca4b002e73f
Leandro Miranda Fahur Machado <fahurleandro@gmail.com>
2022-10-18 22:09:51 -0700

    basic monster-rolodex understanding

---
+++ monsters-rolodex/src/App.js
@@ -0,0 +1,40 @@
+import { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      monsters: [
+        {
+          name: "Bob",
+        },
+        {
+          name: "Fred",
+        },
+        {
+          name: "Jack",
+        },
+        {
+          name: "Andrew",
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.monsters.map((monster, index) => {
+          return (
+            <div key={index}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default App;
