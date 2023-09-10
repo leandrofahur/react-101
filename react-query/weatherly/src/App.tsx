9f2ac2a86839a5d826f3dbc4e132b41f97339c64
leandro <fahurleandro@gmail.com>
2023-09-09 21:35:47 -0700

    weatherly app boilerplate

--- react-query/weatherly/src/App.tsx
+++ react-query/weatherly/src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
+const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
 
 function App() {
-  return <h1>Weatherly App</h1>;
+  return (
+    <>
+      <h1>Weatherly App</h1>
+      <p>API key: {apiKey}</p>
+    </>
+  );
 }
 
 export default App;
