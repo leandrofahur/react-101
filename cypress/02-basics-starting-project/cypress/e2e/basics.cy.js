dd9f2e599761e5772ed312c029d6507acf7f239d
leandro <fahurleandro@gmail.com>
2023-09-29 08:09:56 -0700

    autocompletion for cypress

--- cypress/02-basics-starting-project/cypress/e2e/basics.cy.js
+++ cypress/02-basics-starting-project/cypress/e2e/basics.cy.js
@@ -1,5 +1,8 @@
-describe("template spec", () => {
-  it("passes", () => {
+/// <reference types="Cypress" />;
+
+describe("tasks page", () => {
+  it("should render the main image", () => {
     cy.visit("http://127.0.0.1:5173/");
+    cy.get();
   });
 });
