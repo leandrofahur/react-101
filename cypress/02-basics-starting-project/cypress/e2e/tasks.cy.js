d6cb9bd8507c75b2d6d16c84163ba3a1e4293e71
leandro <fahurleandro@gmail.com>
2023-09-29 09:13:50 -0700

    proper tests take into consideration several different flows and conditions

--- cypress/02-basics-starting-project/cypress/e2e/tasks.cy.js
+++ cypress/02-basics-starting-project/cypress/e2e/tasks.cy.js
@@ -1,11 +1,19 @@
 /// <reference types="Cypress" />;
 
 describe("tasks management", () => {
-  it("should open and close the new task modal", () => {
+  it("should open and close the new task modal by clicking on the backdrop", () => {
     cy.visit("http://127.0.0.1:5173/");
     cy.contains("Add Task").click();
     cy.get(".modal").should("be.visible");
     cy.get(".backdrop").click({ force: true });
+    cy.get(".backdrop").should("not.exist");
+    cy.get(".modal").should("not.exist");
+  });
+  it("should open and close the new task modal by clicking the cancel button", () => {
+    cy.visit("http://127.0.0.1:5173/");
+    cy.contains("Add Task").click();
+    cy.get(".modal").should("be.visible");
+    cy.contains("Cancel").click();
     cy.get(".backdrop").should("not.exist");
     cy.get(".modal").should("not.exist");
   });
