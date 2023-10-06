f4a1cc0d564d19e6edcb369a90c7b0a4e7d4a598
leandro <fahurleandro@gmail.com>
2023-10-06 10:09:05 -0700

    test without repeating code and for more ocmplex behaviour

--- cypress/02-basics-starting-project/cypress/e2e/tasks.cy.js
+++ cypress/02-basics-starting-project/cypress/e2e/tasks.cy.js
@@ -32,4 +32,25 @@ describe("tasks management", () => {
     cy.get(".task h2").contains("New Task");
     cy.get(".task p").contains("New Task Description");
   });
+
+  it("should validate user input", () => {
+    cy.visit("http://127.0.0.1:5173/");
+    cy.contains("Add Task").click();
+    cy.get(".modal").should("be.visible");
+    cy.get(".modal").contains("Add Task").click();
+    cy.contains("Please provide");
+  });
+
+  it("should filter tasks", () => {
+    // there is no need to repeat part of previous tests (code)L
+    cy.visit("http://127.0.0.1:5173/");
+    cy.contains("Add Task").click();
+    cy.get("#title").type("New Task");
+    cy.get("#summary").type("New Task Description");
+    cy.get("#category").select("important");
+    cy.get(".modal").contains("Add Task").click();
+    cy.get(".task").should("have.have.length", 1);
+    cy.get("#filter").select("urgent");
+    cy.get(".task").should("have.have.length", 0);
+  });
 });
