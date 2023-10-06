8ff4e34e5d1d707459c130d667c51b2a0e3dc080
leandro <fahurleandro@gmail.com>
2023-10-06 09:56:01 -0700

    test with typing

--- cypress/02-basics-starting-project/cypress/e2e/tasks.cy.js
+++ cypress/02-basics-starting-project/cypress/e2e/tasks.cy.js
@@ -9,6 +9,7 @@ describe("tasks management", () => {
     cy.get(".backdrop").should("not.exist");
     cy.get(".modal").should("not.exist");
   });
+
   it("should open and close the new task modal by clicking the cancel button", () => {
     cy.visit("http://127.0.0.1:5173/");
     cy.contains("Add Task").click();
@@ -16,5 +17,19 @@ describe("tasks management", () => {
     cy.contains("Cancel").click();
     cy.get(".backdrop").should("not.exist");
     cy.get(".modal").should("not.exist");
+  });
+
+  it("should create a new task", () => {
+    cy.visit("http://127.0.0.1:5173/");
+    cy.contains("Add Task").click();
+    cy.get(".modal").should("be.visible");
+    cy.get("#title").type("New Task");
+    cy.get("#summary").type("New Task Description");
+    cy.get(".modal").contains("Add Task").click();
+    cy.get(".modal").should("not.exist");
+    cy.get(".backdrop").should("not.exist");
+    cy.get(".task").should("have.have.length", 1);
+    cy.get(".task h2").contains("New Task");
+    cy.get(".task p").contains("New Task Description");
   });
 });
