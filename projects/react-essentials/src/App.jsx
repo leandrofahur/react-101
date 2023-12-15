9bda1283d5f3d3e46c0af0a3c1b83e9eef0df8b3
Leandro Miranda Fahur Machado <fahurleandro@gmail.com>
2023-12-14 19:01:04 -0800

    updating the file structure

--- projects/react-essentials/src/App.jsx
+++ projects/react-essentials/src/App.jsx
@@ -1,37 +1,6 @@
-import reactImg from "./assets/react-core-concepts.png";
+import Header from "./components/Header/Header.jsx";
+import CoreConcept from "./components/CoreConcept/CoreConcept.jsx";
 import { CORE_CONCEPTS } from "./data";
-
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcepts(props) {
-  const { title, description, image } = props;
-
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
 
 function App() {
   return (
@@ -42,7 +11,7 @@ function App() {
           <h2>Core Concepts</h2>
           <ul>
             {CORE_CONCEPTS.map((concept) => (
-              <CoreConcepts key={concept.id} {...concept} />
+              <CoreConcept key={concept.id} {...concept} />
             ))}
           </ul>
         </section>
