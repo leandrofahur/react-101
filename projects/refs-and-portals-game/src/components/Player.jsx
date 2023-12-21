ee50aeab2fb19f725bb30973013605997e6eae5c
Leandro Miranda Fahur Machado <fahurleandro@gmail.com>
2023-12-20 23:48:56 -0800

    update

--- projects/refs-and-portals-game/src/components/Player.jsx
+++ projects/refs-and-portals-game/src/components/Player.jsx
@@ -1,11 +1,21 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  const playerName = useRef(null);
+  const [name, setName] = useState("");
+
+  const handleOnClick = () => {
+    setName(playerName.current.value);
+    playerName.current.value = ""; // very careful here! This is a imperative way of doing things. We should avoid it as much as possible.
+  };
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {name ?? "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerName} type="text" />
       </p>
+      <button onClick={handleOnClick}>Set Name</button>
     </section>
   );
 }
