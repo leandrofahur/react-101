62c83c4cda60dadd4a0e06ca73e523884b50e57a
Leandro Miranda Fahur Machado <fahurleandro@gmail.com>
2023-12-21 17:48:43 -0800

    using timer

--- projects/refs-and-portals-game/src/components/TimerChallenge.jsx
+++ projects/refs-and-portals-game/src/components/TimerChallenge.jsx
@@ -1,16 +1,41 @@
+import { useState, useRef } from "react";
+
 export default function TimerChallenge(props) {
   const { title, targetTime } = props;
+
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+
+  const timerId = useRef(null);
+
+  function handleStart() {
+    timerId.current = setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+
+    setTimerStarted(true);
+  }
+
+  function handleStop() {
+    clearTimeout(timerId.current);
+    setTimerStarted(false);
+  }
 
   return (
     <div className="challenge">
       <h2>{title}</h2>
+      {timerExpired && <p className="challenge-expired">You lost!</p>}
       <p className="challenge-time">
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p className="">
-        <button>Start Challenge</button>
+        <button onClick={timerStarted ? handleStop : handleStart}>
+          {timerStarted ? "Stop" : "Start"} Challenge
+        </button>
       </p>
-      <p className="">Time is running... / Timer inactive</p>
+      <p className={timerStarted ? "active" : undefined}>
+        {timerStarted ? "Time is runnning..." : "Timer inactive"}
+      </p>
     </div>
   );
 }
