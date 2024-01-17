03af36ac995ad6dc91bc6a1207df5198d3d6cd3f
Leandro Miranda Fahur Machado <fahurleandro@gmail.com>
2024-01-16 19:58:54 -0800

    it was fun

--- projects/management-app/src/components/ProjectsSidebar.jsx
+++ projects/management-app/src/components/ProjectsSidebar.jsx
@@ -14,7 +14,7 @@ export default function ProjectsSidebar(props) {
       <ul>
         {projects.map((project) => (
           <li key={project.id}>
-            <button className="w-full py-2 text-left hover:bg-stone-800">
+            <button className="w-full text-left px-2 py-1 rounded-sm my-1 text-stone-400  hover:text-stone-200 hover:bg-stone-800">
               {project.title}
             </button>
           </li>
